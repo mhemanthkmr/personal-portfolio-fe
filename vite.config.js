4734679d013b6e07fc4481c053c3ab81fe76d1c8
HemanthKumar M <hemanthkumarava@gmail.com>
2025-07-20 16:45:17 +0530

    Added base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/personal-portfolio-fe/', // 👈 set base to repo name with slashes
   plugins: [react()],
-})
+});
